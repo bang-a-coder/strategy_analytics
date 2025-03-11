397ec1b62662173ba33212da41fb8273211d9672
bang-a-coder <nikitas4717@gmail.com>
2025-03-11 12:23:03 +0100

    Add data assimilation script for merging feelings and text analysis datasets

---
+++ Assignment 2/src/data/assimilate.py
@@ -0,0 +1,37 @@
+#%%
+import pandas as pd
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# %%
+
+df_feelings = pd.read_csv("../../data/processed/feelings/feelings_results_parsed.csv")
+df_lite = pd.read_csv("../../data/interim/text-analysis/brunch_reviews_text_analysis_lite.csv")
+df_full = pd.read_csv("../../data/interim/text-analysis/brunch_reviews_text_analysis.csv")
+# %%
+
+df_lite.head()
+
+# %%
+
+df_feelings = df_feelings.add_prefix('feeling_')
+
+# %%
+
+df_lite = pd.concat([df_lite, df_feelings], axis=1)
+
+# %%
+# df_lite.to_csv("../../data/processed/brunch_reviews_text_analysis_lite.csv", index=False)
+# %%
+df_feelings.columns
+# %%
+df_attributes = pd.read_csv("../../data/processed/business_details_with_attributes.csv")
+# %%
+
+df_reviews_with_business_details = pd.read_csv("../../data/interim/brunch_reviews_with_business_details.csv")
+
+# %%
+df_reviews_with_business_details.drop(columns=['text'], inplace=True)
+# %%
